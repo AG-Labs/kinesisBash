6a9fdbb14a8f4970616e944792e70ff26dd01047
PatelMinal <43324832+PatelMinal@users.noreply.github.com>
2019-09-10 16:46:06 +0100

    Create gohd.sh

---
+++ gohd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export AWS_ACCESS_KEY_ID= <key>
+export AWS_SECRET_ACCESS_KEY= <key>
+export AWS_DEFAULT_REGION=eu-west-1
+
+cd amazon-kinesis-video-streams-producer-sdk-cpp/kinesis-video-native-build
+./kinesis_video_gstreamer_sample_app -h 1920 -w 1080 -f 30 <stream name>
