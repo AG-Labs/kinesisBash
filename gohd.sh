ed9b887db36441dc05626ee6b06f60db80f22d7c
Andrew Godley <30706188+AG-Labs@users.noreply.github.com>
2019-09-11 13:38:05 +0100

    updated input to sample app

--- gohd.sh
+++ gohd.sh
@@ -5,4 +5,4 @@ export AWS_SECRET_ACCESS_KEY= <key>
 export AWS_DEFAULT_REGION=eu-west-1
 
 cd amazon-kinesis-video-streams-producer-sdk-cpp/kinesis-video-native-build
-./kinesis_video_gstreamer_sample_app -h 1920 -w 1080 -f 30 <stream name>
+./kinesis_video_gstreamer_sample_app <stream name> -h 1920 -w 1080 -f 30 
